6c5088f22defcd89d6883b87f62026eb6c5046f5
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-07 19:07:48 -0300

    Fix: login.css import path

--- src/Pages/Login/login.jsx
+++ src/Pages/Login/login.jsx
@@ -1,28 +1,50 @@
-import "./Login.css" ;
-export default function Login(){
-    return(
-<section className="flex flex-col justify-center items-center h-[80vh]">
-    <div >
-        <form action="login" className="w-[100vw]: px-4 h-screen flex flex-col justify-center  md:w-[100%] text-base md:text-[20px]   ">
+import "./login.css";
+export default function Login() {
+  return (
+    <section className="flex flex-col justify-center items-center h-[80vh]">
+      <div>
+        <form
+          action="login"
+          className="w-[100vw]: px-4 h-screen flex flex-col justify-center  md:w-[100%] text-base md:text-[20px]   "
+        >
+          <label htmlFor="Email" className="">
+            Email:
+          </label>
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            required="@gmail.com"
+            className="inputRegistreUser md:min-w[20rem]	 "
+          />
 
-            <label htmlFor="Email" className="" >Email:</label>
-                <input type="email" placeholder= "example@gmail.com" required="@gmail.com" className="inputRegistreUser md:min-w[20rem]	 " />
-            
-            <label htmlFor="Password">Password: </label>
-        
-                <input type="password" placeholder= "Password" required="" className=" inputRegistreUser " />
+          <label htmlFor="Password">Password: </label>
 
-            <input type="submit" value="Login" className="inputRegistreUser  rounded outline outline-1 bg-[#001F3F] text-white	 " />
+          <input
+            type="password"
+            placeholder="Password"
+            required=""
+            className=" inputRegistreUser "
+          />
 
-            <button className="  inputRegistreUser inline-flex items-center justify-center">
-                <picture> <img src="https://imagepng.org/wp-content/uploads/2019/08/google-icon.png" alt="google-icon" className=" w-6 h-6 mx-4 " />
-                </picture> 
-                Login with Google</button>
+          <input
+            type="submit"
+            value="Login"
+            className="inputRegistreUser  rounded outline outline-1 bg-[#001F3F] text-white	 "
+          />
 
-            
-        </form> 
-    </div>
-</section>
-    )
-    
+          <button className="  inputRegistreUser inline-flex items-center justify-center">
+            <picture>
+              {" "}
+              <img
+                src="https://imagepng.org/wp-content/uploads/2019/08/google-icon.png"
+                alt="google-icon"
+                className=" w-6 h-6 mx-4 "
+              />
+            </picture>
+            Login with Google
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
