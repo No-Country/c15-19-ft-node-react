214a43f11a3878aaaeefe93a29816211c5ad282b
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-11 18:29:06 -0300

    fix: resolver conflictos de nomenclatura

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
-import "./Index.css";
+import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
 
