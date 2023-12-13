cc4b347afaaea0b679fd872d0de4b3d64a0c6cb5
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2023-12-13 12:14:14 -0400

    style: Cambio de bg de NavBarLower de BlueMarine a Green

--- src/components/NavBarLower/NavBarLower.jsx
+++ src/components/NavBarLower/NavBarLower.jsx
@@ -8,11 +8,11 @@ import { IoPerson } from "react-icons/io5";
 function NavBarLower() {
     return (
         <div className="fixed bottom-0 h-16 w-screen m-0 flex flex-row items-center justify-around bg-Green text-white shadow-lg">
-            <a href="/"><NavBarInfIcon icon={<FaHouse size="20" />} text='Home' /></a>
-            <a href="/"><NavBarInfIcon icon={<MdMessage size="20" />} text='Messages' /></a>
+            <a href="/home"><NavBarInfIcon icon={<FaHouse size="20" />} text='Home' /></a>
+            <a href="/categories"><NavBarInfIcon icon={<MdMessage size="20" />} text='Categories' /></a>
             <a href="/"><NavBarInfNew icon={<FaPlus size="40" />} text='New' /></a>
             <a href="/"><NavBarInfIcon icon={<BsFillPeopleFill size="20" />} text='Friends' /></a>
-            <a href="/"><NavBarInfIcon icon={<IoPerson size="20" />} text='Profile' /></a>
+            <a href="/profile"><NavBarInfIcon icon={<IoPerson size="20" />} text='Profile' /></a>
         </div>
     );
 }
