6bcc00b2d347a0e33d2e949e61df3323c00bd103
Mek <mgdesampaio@gmail.com>
2023-12-14 13:43:47 -0500

    fix:arreflar userposts

--- src/Pages/UserPosts/UserPosts.jsx
+++ src/Pages/UserPosts/UserPosts.jsx
@@ -1,20 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./useposts.css";
 export const UserPosts = (props) => {
   const usersProp = props.user;
 
   return (
     <div className="parent">
-      {/* {usersProp.challenges?.map((chal) =>
-          chal.media?.map((med) => (
-            <img
-              key={""}
-              className=""
-              src={med.url}
-              alt={""}
-            />
-          ))
-        )} */}
       <img
         className="lg:h-80 w-auto h-40 md:h-60 lg:w-[25vw]"
         src="https://res.cloudinary.com/drw6ofniz/image/upload/v1702487742/test-image/biozywa8fk3nmmvozqfm.jpg"
