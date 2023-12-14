62fc63d8d2da1497cbb00e646f7842b64b9d0670
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-14 01:27:01 -0300

    feat: Crear la petición a la api para renderizar la data del server

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,36 +1,30 @@
-// services/api.js
+export default class Api {
+  constructor() {}
 
-const BASE_URL = "https://api.example.com";
-
-// Ejemplo de función para realizar una solicitud GET
-const fetchData = async (endpoint) => {
-  try {
-    const response = await fetch(`${BASE_URL}/${endpoint}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al obtener datos de la API:", error);
-    throw error;
-  }
-};
-
-// Ejemplo de función para realizar una solicitud POST
-const postData = async (endpoint, body) => {
-  try {
-    const response = await fetch(`${BASE_URL}/${endpoint}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Puedes incluir headers de autenticación u otros según sea necesario
-      },
+  async _useFetch(url, method, body) {
+    const res = await fetch(url, {
+      headers: { "Content-Type": "application/json" },
+      method,
       body: JSON.stringify(body),
     });
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error al enviar datos a la API:", error);
-    throw error;
-  }
-};
 
-export { fetchData, postData };
+    if (res.ok) {
+      return res.json();
+    }
+
+    return Promise.reject(`Error ${res.status}`);
+  }
+
+  async getCardsChallenges() {
+    try {
+      const res = await this._useFetch(
+        "https://challenge-me-backend-uu82.onrender.com/challenges",
+        "GET"
+      );
+
+      return res;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,28 @@
 import CardUser from "../../components/CardUser/CardUser";
-
-import React from "react";
-import tempData from "./temp-data";
+import React, { useEffect, useState } from "react";
+import Api from "../../utils/api";
 
 function Home() {
+  const [cards, setCards] = useState([]);
+
+  const api = new Api();
+
+  useEffect(() => {
+    api
+      .getCardsChallenges()
+      .then((data) => {
+        console.log(data);
+        setCards(data);
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+  }, []);
+
   return (
-    <div>
-      {tempData.map((element) => {
-        return <CardUser key={element.avatar} {...element} />;
+    <div className="relative z-0">
+      {cards.map((element) => {
+        return <CardUser key={element._id} {...element} />;
       })}
     </div>
   );
