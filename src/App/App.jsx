fd74b8da8d772591b9bbf428aa304755a0e6d08f
Mek <mgdesampaio@gmail.com>
2023-12-15 12:37:47 -0500

    feat:agregar error page y rutearla

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,44 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "../layout/Header/Header";
-import NavBarLower from "../components/NavBarLower/NavBarLower";
 import Signin from "../pages/Signin/Signin";
 import Login from "../pages/Login/Login";
 import Home from "../pages/Home/Home";
 import Categories from "../pages/Categories/Categories";
 import Category from "../pages/Categories/Category";
 import Profile from "../pages/Profile/Profile";
-import Achievements from "../pages/Achievements/Achievements";
-import Sync from "../pages/Sync/Sync";
-import Privacy from "../pages/Privacy/Privacy";
-import Security from "../pages/Security/Security"
 import { ContextProvider } from "../context/useContext";
 import CrearPost from "../pages/CreatePost/CreatePost";
+import Error from "../pages/Error/Error";
+import Wrapper from "../pages/Wrapper/Wrapper";
 
 export default function App() {
   return (
     <Router>
       <ContextProvider>
-        <Header />
         <Routes>
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/profile/" element={<Profile />} />
-          <Route path="/profile/achievements" element={<Achievements/>} />
-          <Route path="/profile/sync" element={<Sync/>}/>
-          <Route path="/profile/privacy" element={<Privacy/>}/>
-          <Route path="/profile/security" element={<Security/>}/>
-
-          <Route path="/profile/:id" element={<Profile />} />
-
-          <Route path="/categories/:name" element={<Category />} />
-
-          <Route path="/createpost" element={<CrearPost />} />
-          {/* Agrega más rutas según sea necesario */}
+          <Route exact path="/" element={<Wrapper />}>
+            <Route index element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/categories/:name" element={<Category />} />
+            <Route path="/createpost" element={<CrearPost />} />
+            <Route path="*" element={<Error />} />
+          </Route>
         </Routes>
-        <NavBarLower />
       </ContextProvider>
     </Router>
   );
