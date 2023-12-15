1f044521e25dd692cae563e85d2a7c3ede5612f9
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-15 15:16:02 -0300

    Style: Agregar estilo a toda la card para desktop, agregar estado para el boton comments

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -4,31 +4,34 @@ import {
   CommentOutlined,
   ShareAltOutlined,
   MoreOutlined,
-  UserAddOutlined,
+  UserOutlined,
   LikeTwoTone,
 } from "@ant-design/icons";
 import { Card, Space, Avatar, Image } from "antd";
 import "./CardUser.css";
 
-const { Meta } = Card;
-
 function CardUser(data) {
-  const { avatar, title, media, description, comments } = data;
+  const { avatar, title, media, description } = data;
 
   const [likes, setLike] = useState(false);
+  const [showComments, setShowComments] = useState(false);
 
   const handleCardLike = () => {
     setLike(!likes);
+  };
+
+  const handleShowComments = () => {
+    setShowComments(!showComments);
+    console.log("hola estoy pulsando el boton", !showComments);
   };
 
   const imagePlaceholder =
     "https://www.cronobierzo.es/wp-content/uploads/2020/01/no-image.jpg";
 
   return (
-    <div className="flex flex-col m-4">
+    <div className="flex flex-col m-4 card-user__container">
       <Space direction="vertical" size={16}>
         <Card
-          size="small"
           bordered={false}
           style={{
             width: "100%",
@@ -36,41 +39,42 @@ function CardUser(data) {
           }}
         >
           <>
-            <div className="flex justify-between">
+            <div className="flex justify-between min-w-full">
               <Avatar
+                className="card-user__avatar"
                 src={avatar}
-                icon={<UserAddOutlined />}
+                icon={<UserOutlined className="card-user__avatar-icon " />}
                 style={{ backgroundColor: "#87d068" }}
               />
-              <MoreOutlined />
+              <MoreOutlined className="card-user__icon-lined" />
             </div>
 
-            <div className="items-start cardUserDescription">
-              <Meta title={title} description={description} />
+            <div className="items-start card-user__description">
+              <h2 className="card-user__title"> {title} </h2>
+              <p className="card-user__subtitle"> {description} </p>
             </div>
 
-            <div className="cardUserImageContainer">
+            <div className="card-user__image-container">
               <Image
-                alt="example"
+                alt={title}
                 src={media[0]?.url || imagePlaceholder}
                 className="object-cover	w-full h-full"
               />
             </div>
-            <div className="flex justify-start cardUserSocial">
+            <div className="flex justify-start card-user__social">
               <button onClick={handleCardLike}>
                 {likes ? (
-                  <LikeTwoTone size={15} className="cardUserIcon" />
+                  <LikeTwoTone size={15} className="card-user__icon" />
                 ) : (
-                  <LikeOutlined className="cardUserIcon" size={10} />
+                  <LikeOutlined className="card-user__icon" />
                 )}
-                {likes ? "1" : "0"}
+                <span className="card-user__number">{likes ? "1" : "0"}</span>
               </button>
 
-              <div>
-                <CommentOutlined className="cardUserIcon" size={10} />
-                {comments}
-              </div>
-              <ShareAltOutlined size={25} />
+              <button type="button" onClick={handleShowComments}>
+                <CommentOutlined className="card-user__icon card-user__icon_margin" />
+              </button>
+              <ShareAltOutlined className="card-user__icon card-user__icon_margin" />
             </div>
           </>
         </Card>
