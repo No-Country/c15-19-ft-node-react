9e9f02385ca4103d342caa16d4d4cef8b6b736f0
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-20 14:36:33 -0300

    feat: Crear landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import logo from "../../images/logo.png";
 import { useNavigate } from "react-router-dom";
+import "./landing-page.css";
 
 function LandingPage() {
   const navigate = useNavigate();
@@ -19,23 +20,28 @@ function LandingPage() {
   };
 
   return (
-    <section className="flex flex-row items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4 ">ChallengeMe</h1>
-        <p className="mb-4">
-          Bienvenido a <strong>ChallengeMe</strong>, la aplicación que
-          transforma la diversión en una experiencia compartida y emocionante.
-          En ChallengeMe, no solo creas y compartes desafíos, sino que también
-          construyes conexiones significativas a través de tribus vibrantes.
-        </p>
-        <p className="mb-8">
-          Imagina una plataforma donde la diversión se convierte en un lenguaje
-          universal. ChallengeMe se creó con la visión de unir a las personas de
-          manera única y dinámica mediante desafíos creativos que fomentan la
-          camaradería y el espíritu competitivo.
-        </p>
+    <section className=" landing-page">
+      <img className="landing-page__logo" src={logo} alt="Logo ChallengeMe" />
 
+      <div className="landing-page__container">
         <div>
+          <h1 className="landing-page__title ">ChallengeMe</h1>
+          <p className="landing-page__paragraph ">
+            Bienvenido a <strong>ChallengeMe</strong>, la aplicación que
+            transforma la diversión en una experiencia compartida y emocionante.
+            En ChallengeMe, no solo creas y compartes desafíos, sino que también
+            construyes conexiones significativas a través de tribus vibrantes.
+          </p>
+          <br />
+          <p className="landing-page__paragraph ">
+            Imagina una plataforma donde la diversión se convierte en un
+            lenguaje universal. ChallengeMe se creó con la visión de unir a las
+            personas de manera única y dinámica mediante desafíos creativos que
+            fomentan la camaradería y el espíritu competitivo.
+          </p>
+        </div>
+
+        <div className="landing-page__button">
           <button
             onClick={handleShowLogin}
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4"
@@ -50,8 +56,6 @@ function LandingPage() {
           </button>
         </div>
       </div>
-
-      <img className="mt-8" src={logo} alt="Logo ChallengeMe" />
     </section>
   );
 }
