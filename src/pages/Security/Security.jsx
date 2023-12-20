e8b61a521c089ef4de23df65c28b0ddc41d3b54d
Alejandro <alejandroaml0528@gmail.com>
2023-12-20 14:25:39 -0400

    migration

--- src/pages/Security/Security.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function Security() {
-  useEffect(() => {
-    console.log("use Effect");
-  }, []);
-
-  return <h1>Holis soy seguridad</h1>;
-}
--- src/pages/Sync/Sync.jsx
+++
@@ -1,3 +0,0 @@
-export default function Sync() {
-    return <h1>Hola Soy sincronizacion</h1>
-}
