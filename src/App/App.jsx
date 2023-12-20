5d59969e2ccd952d297d1884b7ffcbcb5967d534
Mary Martinez <isa@MacBook-Air-de-Mary.local>
2023-12-20 12:28:18 -0300

    fix: agregar ruta de comentarios en App.js

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -18,44 +18,46 @@ import CrearPost from "../pages/CreatePost/CreatePost";
 import Error from "../pages/Error/Error";
 import Wrapper from "../pages/Wrapper/Wrapper";
 import AuthLayout from "../pages/Wrapper/AuthLayout";
-import Comment from "../pages/Comments/Comment";
+import Comments from "../pages/Comments/Comments";
 import VerifyAccount from "../pages/VerifyAccount/VerifyAccout";
 
 export default function App() {
   return (
     <Router>
-    
+      <ContextProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="/verify-account" element={<VerifyAccount />} />
+            </Route>
+            <Route path="/" element={<Wrapper />}>
+              <Route path="home" element={<Home />} />
+              <Route path="home/comments" element={<Comments />} />
+              <Route path="/categories" element={<Categories />} />
+              {/* <Route path="/profile" element={<Profile />} /> */}
+              <Route path="/profile/achievements" element={<Achievements />} />
+              <Route path="/profile/sync" element={<Sync />} />
+              <Route path="/profile/privacy" element={<Privacy />} />
+              <Route path="/profile/security" element={<Security />} />
 
-          <ContextProvider>
-          <AuthProvider>
-            <Routes>
-                  <Route path='/' element={<AuthLayout/>}>
-                        <Route index element={<Login />} />
-                        <Route path="register" element={<Register />} />
-                        <Route path="/verify-account" element={<VerifyAccount />}/>
-                  </Route>
-                 <Route  path="/" element={<Wrapper/>}>
-                  <Route path="home" element={<Home />} />
-                  <Route path="/categories" element={<Categories />} />
-                  {/* <Route path="/profile" element={<Profile />} /> */}
-                  <Route path="/profile/achievements" element={<Achievements/>} />
-                  <Route path="/profile/sync" element={<Sync/>}/>
-                  <Route path="/profile/privacy" element={<Privacy/>}/>
-                  <Route path="/profile/security" element={<Security/>}/>
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route path="/profile/:id/privacy" element={<Privacy />} />
+              <Route
+                path="/profile/:id/editprofile"
+                element={<EditProfile />}
+              />
 
-                  <Route path="/profile/:id" element={<Profile />} />
-                  <Route path="/profile/:id/privacy" element={<Privacy />} />
-                  <Route path="/profile/:id/editprofile" element={<EditProfile />} />
+              <Route path="/categories/:name" element={<Category />} />
 
-                  <Route path="/categories/:name" element={<Category />} />
+              <Route path="/createpost" element={<CrearPost />} />
 
-                  <Route path="/createpost" element={<CrearPost />} />
-                
-                  <Route path="*" element={<Error />} />
-              </Route>
-            </Routes>
-            </AuthProvider>
-          </ContextProvider>
-      </Router>
+              <Route path="*" element={<Error />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </ContextProvider>
+    </Router>
   );
 }
